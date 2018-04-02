f27042f18b0c3184211504f627a007458734b43e
cyruslk <cyrus7@hotmail.fr>
2018-04-02 15:11:27 -0400

    Adding the update and getting all the CRUD cycle

---
+++ playground/mongodb-update.js
@@ -0,0 +1,31 @@
+// const MongoClient = require("mongodb").MongoClient;
+const {
+    MongoClient,
+    ObjectID
+} = require("mongodb");
+
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+    if (err) {
+        return console.log("Unable to connect to MongoDB server");
+    }
+    console.log("Connected to MongoDB server");
+
+    db.collection('Todos').findOneAndUpdate({
+      _id: new ObjectID("5ac26de61791d8688671ce99")
+    }, {
+      $set: {
+        completed: false
+      },
+      // It add a new category and increment it
+      $inc: {
+        age: 26
+      }
+    }, {
+      returnOriginal: false
+    }).then((result) => {
+      console.log(result);
+    })
+
+    db.close();
+});
